a49ccba805853fe32aba4509867adc6cc402e5f8
DonatasNoreika <donoras@gmail.com>
2026-04-14 09:46:07 +0300

    + laiko zonos nustatymas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -109,12 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'lt'
 
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
 
-USE_TZ = True
+# TIME_ZONE = 'UTC'
+TIME_ZONE = "Europe/Vilnius"
 
+# USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
